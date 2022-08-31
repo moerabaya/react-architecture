76a3b4337ab1bf48bab074ad0e63916e4c2edf45
Mohammed Rabaya <rabaya.moe@gmail.com>
2022-08-31 21:49:00 +0300

    Fix React not imported on CounterButton

--- src/state-management/components/CounterButton.js
+++ src/state-management/components/CounterButton.js
@@ -1,5 +1,5 @@
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 const CounterButton = ({numberOfClicks, increment}) => {
 	const [incrementBy, setIncrementBy] = useState(1);
