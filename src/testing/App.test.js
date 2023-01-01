f73bbe51d4c36c43a9e5797506b2083b8d014dc6
Mohammed Rabay'a <rabaya.moe@gmail.com>
2023-01-01 15:42:15 +0100

    Fix running tests

--- src/testing/App.test.js
+++ src/testing/App.test.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Tests from './index';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Tests />, div);
+import React from "react";
+import Tests from "./index";
+import { render, screen } from "@testing-library/react";
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  render(<Tests />, div);
 });
